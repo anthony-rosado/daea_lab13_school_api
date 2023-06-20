6eb504f2aa7318e8784aa20dcc420be681a9b328
ZeroBlackErron <arosado-26@hotmail.com>
2023-06-19 21:19:28 -0500

    Agregar archivos de proyecto.

---
+++ Lab13StudentsApi/Controllers/StudentsController.cs
@@ -0,0 +1,63 @@
+﻿using Lab13StudentsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab13StudentsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly SourceContext _context;
+
+        public StudentsController(SourceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetInvoices()
+        {
+            if (_context.Students == null)
+            {
+                return NotFound();
+            }
+            return await _context.Students.ToListAsync();
+        }
+
+        // GET: api/Students/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            if (_context.Students == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // POST: api/Students
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            if (_context.Students == null)
+            {
+                return Problem("Entity set 'SourceContext.Students'  is null.");
+            }
+
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStudent", new { id = student.StudentId }, student);
+        }
+    }
+}
---
+++ Lab13StudentsApi/Models/SourceContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab13StudentsApi.Models
+{
+    public class SourceContext: DbContext
+    {
+        public SourceContext(DbContextOptions<SourceContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
